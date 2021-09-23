5f79f084d7303fd8f1338f8d69a6ead84ff17661
Joseph Panetta <jlpanetta1681@icloud.com>
2021-09-22 21:10:41 -0400

    working on getting past inputs not found in tsconfig file error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
  const express = require( "express" );
  const app = express();
- const port = 8000; // default listen port
+ const port = 8000; // default dev port
 
  // define route handler for default home page
  app.get("/", (req, res) => {
